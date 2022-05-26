5c6e398f840f80b3f0367d61ec216281ca2ffec3
Cathy Oun 🦄 <ouncathy@amazon.com>
2022-05-25 18:39:34 -0700

    Initial Commit

---
+++ Alleyway/AlleywayApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlleywayApp.swift
+//  Alleyway
+//
+//  Created by Oun, Cathy on 5/25/22.
+//
+
+import SwiftUI
+
+@main
+struct AlleywayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
